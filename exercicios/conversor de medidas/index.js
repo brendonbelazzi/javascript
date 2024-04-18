e2f93be5fc52a038d95cb00df1406ef918e4e808
Brendon Belazzi <brendon.lima_2016@hotmail.com>
2024-04-18 14:56:36 -0300

    Update index.js

--- exercicios/conversor de medidas/index.js
+++ exercicios/conversor de medidas/index.js
@@ -7,22 +7,32 @@ let medida = prompt(
 
 switch (medida) {
   case "mm":
-    alert("O resultado em milímetro é " + numero + "m = " * 1000 + "mm");
+    alert(
+      "O resultado em milímetro é " + numero + " m = " + numero * 1000 + " mm"
+    );
     break;
   case "cm":
-    alert("O resultado em centímetros é " + numero + "m = " * 100 + "cm");
+    alert(
+      "O resultado em centímetros é " + numero + " m = " + numero * 100 + " cm"
+    );
     break;
   case "dm":
-    alert("O resultado em decímetro é " + numero + "m = " * 10 + "dm");
+    alert(
+      "O resultado em decímetro é " + numero + " m = " + numero * 10 + " dm"
+    );
     break;
   case "dam":
-    alert("O resultado em decâmetro é " + numero + "m = " / 10 + "dam");
+    alert("O resultado em decâmetro é " + " m = " + numero / 10 + " dam");
     break;
   case "hm":
-    alert("O resultado em hectômetro é " + numero + "m = " / 100 + "hm");
+    alert(
+      "O resultado em hectômetro é " + numero + " m = " + numero / 100 + " hm"
+    );
     break;
   case "km":
-    alert("O resultado em quilômetro é " + numero + "m = " / 1000 + "km");
+    alert(
+      "O resultado em quilômetro é " + numero + " m = " + numero / 1000 + " km"
+    );
     break;
   default:
     alert("Opção inválida.");
